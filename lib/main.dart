ebfcc83f05fe3329fcfeb7a79c037815bb1e095b
Marian Adly <marianadly2004@gmail.com>
2024-02-05 18:21:01 +0200

    add buttons and text

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,156 @@ class PointsCounter extends StatelessWidget {
             style: TextStyle(fontSize: 25, color: Colors.white),
           ),
         ),
+        body: Row(
+          children: [
+            Column(
+              children: [
+                Text(
+                  'Team A',
+                  style: TextStyle(
+                    fontSize: 35,
+                  ),
+                ),
+                Text(
+                  '0',
+                  style: TextStyle(
+                    fontSize: 150,
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(150, 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    primary: Color.fromARGB(255, 83, 146, 180),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Add 1 point',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                const Spacer(
+                  flex: 1,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(150, 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    primary: Color.fromARGB(255, 83, 146, 180),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Add 2 point',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Spacer(
+                  flex: 1,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(150, 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    primary: Color.fromARGB(255, 83, 146, 180),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Add 3 point',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                const Spacer(
+                  flex: 12,
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Text(
+                  'Team B',
+                  style: TextStyle(
+                    fontSize: 35,
+                  ),
+                ),
+                Text(
+                  '0',
+                  style: TextStyle(
+                    fontSize: 150,
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(150, 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    primary: Color.fromARGB(255, 83, 146, 180),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Add 1 point',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Spacer(
+                  flex: 1,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(150, 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    primary: Color.fromARGB(255, 83, 146, 180),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Add 2 point',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Spacer(
+                  flex: 1,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(150, 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    primary: Color.fromARGB(255, 83, 146, 180),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Add 3 point',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Spacer(
+                  flex: 12,
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
